0c747b3035a32e1faa16308928356e515b93db06
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-08-27 09:29:47 -0300

    feat/fix renamed folders

--- react-bootcamp-Julieta/src/App.tsx
+++ react-bootcamp-Julieta/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Content from './Class 3/Lesson_01/Content'
-import Counter from './Class 4/Lesson_01/Contador'
-import Post from './Class 3/Post'
-import TrafficLight from './Class 4/TrafficLight'
-import StockWarning from './Class 4/Stock'
-import NameList from './Class 4/Nombres'
+import Content from './Class3/Lesson_01/Content'
+import Counter from './Class4/Lesson_01/Contador'
+import Post from './Class3/Post'
+import TrafficLight from './Class4/TrafficLight'
+import StockWarning from './Class4/Stock'
+import NameList from './Class4/Nombres'
 
 function App() {
 
